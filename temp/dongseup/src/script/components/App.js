313c31ae53817201a431f8ffe507a6cba4b8c343
dongseup <dongseup83@gmail.com>
2016-09-07 09:59:38 +0900

    리스트 샘플

---
+++ temp/dongseup/src/script/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import List from './List';
+
+class App extends React.Component {
+    render(){
+        return (
+            <List/>
+        );
+    }
+}
+
+export default App;
--- temp/dongseup/src/script/index.js
+++ temp/dongseup/src/script/index.js
@@ -1,3 +1,9 @@
-import '../style/style.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+import '../style/style.scss';
 import App from './components/App';
+
+const rootElement = document.getElementById('root');
+console.log(rootElement);
+ReactDOM.render(<App/>, rootElement);
