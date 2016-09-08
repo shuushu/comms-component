350bf68ece17820bc66482fa6ddb87a966d38748
dongseup <dongseup83@gmail.com>
2016-09-08 09:29:59 +0900

    리스트 이미지 수정

--- temp/dongseup/src/script/components/Contact.js
+++ temp/dongseup/src/script/components/Contact.js
@@ -36,6 +36,64 @@ class Contact extends React.Component{
                     name: '유낙동',
                     phone: '010-2967-1123',
                     absence: 1
+                },
+                {
+                    name: '이승헌',
+                    phone: '010-3484-2922',
+                    accumulate: 2
+                },
+                {
+                    name: '최훈철',
+                    phone: '010-7878-2908',
+                    accumulate: 1
+                },
+                {
+                    name: '이인영',
+                    phone: '010-2648-1909'
+                },
+                {
+                    name: '이상훈',
+                    phone: '010-9098-2958',
+                    absence: 1,
+                    accumulate: 3
+                },
+                {
+                    name: '김율아',
+                    phone: '010-2929-1100'
+                },
+                {
+                    name: '유낙동',
+                    phone: '010-2967-1123',
+                    absence: 1
+                },
+                {
+                    name: '이승헌',
+                    phone: '010-3484-2922',
+                    accumulate: 2
+                },
+                {
+                    name: '최훈철',
+                    phone: '010-7878-2908',
+                    accumulate: 1
+                },
+                {
+                    name: '이인영',
+                    phone: '010-2648-1909'
+                },
+                {
+                    name: '이상훈',
+                    phone: '010-9098-2958',
+                    absence: 1,
+                    accumulate: 3
+                },
+                {
+                    name: '김율아',
+                    phone: '010-2929-1100'
+                },
+                {
+                    name: '유낙동',
+                    phone: '010-2967-1123',
+                    absence: 1
                 }
             ]
         };
